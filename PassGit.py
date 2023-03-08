2335edd82d83a9dc988d09507d27b082930ffd32
Spycrab2 <60488356+Finixpro@users.noreply.github.com>
2023-03-08 12:30:23 -0500

    Update PassGit.py

--- PassGit.py
+++ PassGit.py
@@ -1,20 +1,19 @@
 complete = False
-user = {{"Franky" : "Apple"} , {"Abi" : "Poopyfart"}}
+user = ["Franky" , "Apple" , "Abi" , "Poopyfart"]
 
 while not complete:
     username = input("What is your username? ")
     password = input("What is your password? ")
     conf_username = input("Repeat the username: ")
     conf_password = input("Repeat the password: ")
-
+    
     if username != conf_username or password != conf_password:
         print("username or password does not match!")
-        continue
+        
     if not username in user:
         print("Input username again!")
-    if password == user[username]:
-        if [username] == user.find(username)
+        
+    if user.index(password) != -1:
         print(f"User has been identified, Welcome {username}")
-        complete = True
     else:
         print("Input password again")
